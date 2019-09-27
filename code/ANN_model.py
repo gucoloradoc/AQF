cfb07213c5bc5ae11e6a12ecd112a5817ef54bad
gucoloradoc <zurjak88@gmail.com>
2019-09-27 10:05:41 -0500

    Changes to Sep27

--- code/ANN_model.py
+++ code/ANN_model.py
@@ -1,50 +1,66 @@
 
 import numpy as np
 import pandas as pd
-import kerasb
+import keras
 
 
 dframe=pd.read_csv("data/Monterrey/imputed/data/NOROESTE.csv", 
     parse_dates=["FECHA"], infer_datetime_format=True).set_index("FECHA")
 
-def windows_tensor(dataframe, predictors, target):
+def windows_tensor(dataframe, predictors, target, train_per=0.7, val_per=0.15):
     """
-    This function generates an output array with the shape (0:72, predictors,:), to be used in the observations generator.
+    This function generates the output arrays with the shape (:,0:72, predictors), to be used in the observations generator.
+    Training, validation and testing datasets are returned.
     """
     wind_tensor=np.array([dataframe.iloc[(i-24):(i+48),:].loc[:,predictors].values for i in range(24,len(dataframe),72) if ((i+72)<=len(dataframe))])
     #Convert to array
-    return(wind_tensor)
-
-w_ten=windows_tensor(dframe, dframe.columns.values[5:8],1)
+    rand_indexes=np.random.choice(range(0,wind_tensor.shape[0]),size=wind_tensor.shape[0],replace=False)
+    train_indexes=rand_indexes[0:int(np.ceil(train_per*wind_tensor.shape[0]))]
+    val_indexes=rand_indexes[int(np.ceil(train_per*wind_tensor.shape[0])):int(np.ceil((train_per+val_per)*wind_tensor.shape[0]))]
+    test_indexes=rand_indexes[int(np.ceil((train_per+val_per)*wind_tensor.shape[0])):]
+    return(wind_tensor[train_indexes,:,:],wind_tensor[val_indexes,:,:],wind_tensor[test_indexes,:,:])
 
 class observations_generator(keras.utils.Sequence):
     """
     Observation generator, it takes windows of 72 hours and from such widows it generates random observations, to predict 24 lead time taing 24 hours before.
-    data_source is a str with the location of a complete dataset
+    data_source is an array with the location of a complete dataset
     station is the name of the measurement station
     predictors is an array of the desired predictor to be taken into account for the prediction model
     target is the variable to be predicted
     batch_size in the size of the batches to be generated
     """
 
-    def __init__(self, data_source=pd.read_csv("data/Monterrey/imputed/data/NOROESTE.csv", parse_dates=["FECHA"], infer_datetime_format=True).set_index("FECHA"), station, predictors, target, samples_per_window=5, batch_size=64):
+    def __init__(self, 
+    data_source, station, predictors, target, 
+    samples_per_window=5, batch_size=64):
         self.data_source=data_source
         self.station=station
         self.predictors=predictors
         self.target=target
-        self.windows_tensor=windows_tensor(data_source, predictors, target)
         self.batch_size=batch_size
         self.samples_per_window=samples_per_window
+        c=0
+        self.indexes=[]
+        for i in range(0, int(self.data_source.shape[0]*self.samples_per_window)):
+            if c==self.samples_per_window:
+                c=0
+            if c==0:
+                temp=i
+            self.indexes.append((temp,np.random.choice(np.array(list(range(24,48))),size=1)[0]))
+            c=c+1
 
-
-        #Generate the tensor with
+    #Generate the tensor with
     def __len__(self):
-        #need to think how to stimate this length
-        return(int(self.windows_tensor.shape[0]/self.batch_size))
+        #Total lenght of the output
+        return(int(self.data_source.shape[0]*self.samples_per_window/self.batch_size))
     
     def __getitem__(self, idx):
-        ##DdDdSDSdSD
-        self.x_batch= self.windows_tensor[idx*self.batch_size:(1+idx)*self.batch_size,:,:]
+        ##Generador de observaciones, ojo aca con el significado de cada bache
+        self.x_batch= train_win[self.indexes[idx][0],list(range((self.indexes[idx][1]-23),self.indexes[idx][1]+1)),:]
+        return 1
+#Testing zone
 
-pivot_index=np.random.choice(np.array(list(range(24,48))),size=1)
-#windows_tensor[0,[(pivot_index-24):pivot_index],:].shape
+train_win,val_win,test_win=windows_tensor(dframe, dframe.columns.values[5:8],1)
+
+pivot_index=np.random.choice(np.array(list(range(24,48))),size=1)[0]
+train_win[:,list(range((pivot_index-23),pivot_index+1)),:].shape
